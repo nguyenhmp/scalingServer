e4392f08f66efca0f1cc1433688fc14e6cfd0c04
Minh Nguyen <nguyenhminhp@gmail.com>
2016-04-20 13:12:17 -0700

    final answer

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
-var mysql = require('mysql');
 var _ = require('underscore')
 var express = require('express'); 
 var async = require('async');
-var server = express();
+var server = express()
+var bodyParser = require('body-parser');
 var randy = require('randy');
 var faker = require('faker');
-var bodyParser = require('body-parser')
+var gzippo = require('gzippo');
 var countries = ["Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra", "Angola", "Anguilla", "Antarctica", "Antigua and Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia and Herzegowina", "Botswana", "Bouvet Island", "Brazil", "British Indian Ocean Territory", "Brunei Darussalam", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Central African Republic", "Chad", "Chile", "China", "Christmas Island", "Cocos (Keeling) Islands", "Colombia", "Comoros", "Congo", "Congo, the Democratic Republic of the", "Cook Islands", "Costa Rica", "Cote d'Ivoire", "Croatia (Hrvatska)", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "East Timor", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Falkland Islands (Malvinas)", "Faroe Islands", "Fiji", "Finland", "France", "France Metropolitan", "French Guiana", "French Polynesia", "French Southern Territories", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Heard and Mc Donald Islands", "Holy See (Vatican City State)", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran (Islamic Republic of)", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Korea, Democratic People's Republic of", "Korea, Republic of", "Kuwait", "Kyrgyzstan", "Lao, People's Democratic Republic", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libyan Arab Jamahiriya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia, The Former Yugoslav Republic of", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Martinique", "Mauritania", "Mauritius", "Mayotte", "Mexico", "Micronesia, Federated States of", "Moldova, Republic of", "Monaco", "Mongolia", "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Niue", "Norfolk Island", "Northern Mariana Islands", "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Pitcairn", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russian Federation", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Seychelles", "Sierra Leone", "Singapore", "Slovakia (Slovak Republic)", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Georgia and the South Sandwich Islands", "Spain", "Sri Lanka", "St. Helena", "St. Pierre and Miquelon", "Sudan", "Suriname", "Svalbard and Jan Mayen Islands", "Swaziland", "Sweden", "Switzerland", "Syrian Arab Republic", "Taiwan, Province of China", "Tajikistan", "Tanzania, United Republic of", "Thailand", "Togo", "Tokelau", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks and Caicos Islands", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "United States Minor Outlying Islands", "Uruguay", "Uzbekistan", "Vanuatu", "Venezuela", "Vietnam", "Virgin Islands (British)", "Virgin Islands (U.S.)", "Wallis and Futuna Islands", "Western Sahara", "Yemen", "Yugoslavia", "Zambia", "Zimbabwe"];
-var dbController = require('./server/controller/dbController')
+var dbController = require('./server/controllers/dbController.js')
+var NodeCache = require( "node-cache" );
+var myCache = new NodeCache();
 
-server.set('view engine', 'ejs');
-server.set('views', __dirname + '/public/views');
-server.use(bodyParser.urlencoded({extended:true}));
+server.set('view engine', 'ejs')
+server.use(bodyParser.urlencoded({extended:true}))
 server.use(bodyParser.json());
-server.use(express.static(__dirname + '/public'));
+server.use(gzippo.staticGzip(__dirname + '/public'));
+server.use(gzippo.compress());
 server.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -21,18 +23,32 @@ server.use(function(req, res, next) {
 });
 
 server.get('/', function(req, res, next){
-  async.parallel({
-    select: dbController.select,
-    topBusinessInCountry: dbController.topBusinessInCountry
-  },
-  function(err, results) {
-    res.status(200).render('index', results)
+  myCache.get( "results", function( err, value ){
+    if( !err ){
+      if(value == undefined){
+        async.parallel({
+          select: dbController.select,
+          topBusinessInCountry: dbController.topBusinessInCountry
+        },
+        function(err, results) {
+          myCache.set( "results", results, function( err, success ){
+            if( !err && success ){
+              res.status(200).render('index', results)
+            }
+          });
+        });
+      }else{
+        res.status(200).render('index', value)
+      }
+    }
   });
 })
 
 server.post('/businesses', function(req, res, next){
-  var number = req.body.insertNumber
+  var number = req.body.insertNumber;
+  console.log(number);
   dbController.insertBusiness(number);
+  res.redirect('/');
 })
 
 server.listen((3000), function(req, res){
