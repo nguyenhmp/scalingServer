84952d21b7f1ca761f9bba047a0cae60b1ddeea1
Minh Nguyen <nguyenhminhp@gmail.com>
2016-05-24 10:07:27 -0700

    code clean up

--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -1,7 +1,5 @@
 var mysql = require('mysql');
 var dbSettings = require('./dbSettings.js');
-var networkSettings = require('./networkSettings.js');
-dbSettings.port = networkSettings.port;
 var dbpool = mysql.createPool(dbSettings);
 module.exports = (function(){
 	return dbpool;
